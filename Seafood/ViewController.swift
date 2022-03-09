0b48280956adb8e6d93e5e65d34a2bce5047da0b
Patryk Piwowarczyk <shepherd90@windowslive.com>
2022-03-09 20:52:12 +0100

    Initial Commit

---
+++ Seafood/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Seafood
+//
+//  Created by Patryk Piwowarczyk on 09/03/2022.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+
+
+
+
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+   
+    
+    let imagePicker = UIImagePickerController()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        
+        
+        
+    }
+
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        self.imageView.image = userPickedImage
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
+}
+    
+
